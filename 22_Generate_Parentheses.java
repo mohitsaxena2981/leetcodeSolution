87a0a00f85b17b45362f7af1bf8d3a961dbe5a7c
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-10-12 10:30:23 +0530

    Create 22_Generate_Parentheses.java

---
+++ 22_Generate_Parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list=new ArrayList<>();
+        paranthesis(n,0,0,"",list);
+        return list;
+        
+    }
+    public static void paranthesis(int n,int opening,int closing,String ans,List<String> list)
+    {
+        if(opening==n && closing==n)
+        {
+            list.add(ans);
+            return;
+        }
+        if(opening<n)
+        {
+            paranthesis(n,opening+1,closing,ans+"(",list);
+        }
+        if(closing<opening)
+        {
+            paranthesis(n,opening,closing+1,ans+")",list);
+        }
+    }
+}
