c60ddf53617aef439f1dfc954bab0e20d0ada9fe
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-10-31 11:13:43 +0530

    Create 713_Subarray_Product_Less_Than_K.java

---
+++ 713_Subarray_Product_Less_Than_K.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int si=0;
+        int ei=0;
+        int ans=0;
+        int p=1;
+        while(ei<nums.length)
+        {
+            p*=nums[ei];
+            while(p>=k && si<=ei)
+            {
+                p/=nums[si];
+                si++;
+            }
+            
+            ans+=ei-si+1;
+            ei++;
+            
+        }
+        return ans;
+        
+    }
+}
