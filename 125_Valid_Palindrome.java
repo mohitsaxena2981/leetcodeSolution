658fe7db6082400c0ee9848798f31e413b2b8610
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-11-07 19:00:06 +0530

    Update 125_Valid_Palindrome.java

--- 125_Valid_Palindrome.java
+++ 125_Valid_Palindrome.java
@@ -3,8 +3,7 @@ class Solution {
         StringBuilder sb=new StringBuilder("");
         for(int i=0;i<s.length();i++){
             if((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='A' && s.charAt(i)<='Z') || (s.charAt(i)>='0' && s.charAt(i)<='9')){
-                sb.append(Character.toLowerCase(s.charAt(i)));
-                
+                sb.append(Character.toLowerCase(s.charAt(i)));          
             }
         }
         for(int i=0;i<sb.length();i++){
