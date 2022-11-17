579baf504f0a4ba6e19c1b13a44c4efe37c5b54f
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-11-17 09:34:41 +0530

    Create 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java

---
+++ 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int c=0;
+        for(int x=0;x<grid.length;x++)
+        {
+            for(int y=0;y<grid[0].length;y++)
+            {
+                if(grid[x][y]<0)
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+        
+    }
+}
