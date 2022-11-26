db6c389af7eb0fc25430e2bc90a20181272f2900
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-11-27 01:02:48 +0530

    Create 1221_Split_a_String_in_Balanced_Strings.java

---
+++ 1221_Split_a_String_in_Balanced_Strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int cr=0;
+        int cl=0;
+        int c=0;
+        for(int x=0;x<s.length();x++)
+        {
+            if(s.charAt(x)=='L')
+            {
+                cl++;
+            }
+            else if(s.charAt(x)=='R')
+            {
+                cr++;
+            }
+            if(cr==cl)
+            {
+                c++;
+                cr=0;
+                cl=0;
+            }
+        }
+        return c;
+        
+    }
+}
