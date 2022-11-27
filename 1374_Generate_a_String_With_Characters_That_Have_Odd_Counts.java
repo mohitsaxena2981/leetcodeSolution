c02d5c8ea249603b5d961361ec148f07109561a0
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-11-28 00:30:36 +0530

    Create 1374_Generate_a_String_With_Characters_That_Have_Odd_Counts.java

---
+++ 1374_Generate_a_String_With_Characters_That_Have_Odd_Counts.java
@@ -0,0 +1,15 @@
+class Solution {
+public String generateTheString(int n) {
+    String str="";
+    if(n%2==0){
+        for(int i=0;i<n-1;i++)
+        str+='m';
+        str+='s';
+    }
+    else{
+        for(int i=0;i<n;i++)
+        str+='m';
+    }
+        return str;
+    }
+}
