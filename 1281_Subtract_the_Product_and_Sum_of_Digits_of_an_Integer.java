55f329c0a33b7aff2830dd4b83f247e523a8891a
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-11-28 02:41:52 +0530

    Create 1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java

---
+++ 1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        long prd=1;
+        long sum=0;
+        while(n>0)
+        {
+            int res=n%10;
+            prd*=res;
+            sum+=res;
+            n/=10;
+        }
+        return ((int)(prd-sum));
+        
+    }
+}
