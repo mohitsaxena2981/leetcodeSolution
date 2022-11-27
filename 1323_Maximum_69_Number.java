a5f1a92ce5f9ac027889656534137ec578e4f3b7
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-11-28 02:39:10 +0530

    Create 1323_Maximum_69_Number.java

---
+++ 1323_Maximum_69_Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String s="";
+        s+=num;
+        s=s.replaceFirst("6","9");
+        return Integer.parseInt(s);
+    }
+}
