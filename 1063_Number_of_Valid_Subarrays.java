b5a7866d5c21ee7a1bcede2cbc975c2e1c62566b
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-12-01 14:10:25 +0530

    Create 1063_Number_of_Valid_Subarrays.java

---
+++ 1063_Number_of_Valid_Subarrays.java
@@ -0,0 +1,22 @@
+import java.util.*;
+  class Solution {
+    public int validSubarrays(int[] nums) {
+      return ValidSubArray(nums);
+    }
+	
+	public static int ValidSubArray(int arr[])
+	{
+	    Stack<Integer> st=new Stack<>();
+	    int ans=0;
+	    for(int x=0;x<arr.length;x++)
+	    {
+	        while(!st.isEmpty() && arr[x]<st.peek())
+	        {
+	            st.pop();
+	        }
+	        st.push(arr[x]);
+	        ans+=st.size();
+	    }
+	    return ans;
+	}
+}
