b1649039950291bc44b7ad1a0869ad04cbc8ead5
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2022-12-28 14:25:26 +0530

    Update 496_Next_Greater_Element_I.java

--- 496_Next_Greater_Element_I.java
+++ 496_Next_Greater_Element_I.java
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int z=-1;
@@ -28,5 +29,40 @@ class Solution {
         }
         return nums;
         
+    }
+}
+
+//Method 2
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int z=0;
+        int c=0;
+        int ans[]=new int[nums1.length];
+        for(int x=0;x<nums1.length;x++)
+        {
+            for(int y=0;y<nums2.length;y++)
+            {
+                if(nums2[y]==nums1[x])
+                {
+                    z=y;
+                    break;
+                }
+            }
+            for(int y=z;y<nums2.length;y++)
+            {
+                if(nums2[y]>nums2[z])
+                {
+                    ans[c]=nums2[y];
+                    break;
+                }
+
+            }
+            if(ans[c]==0)
+            {
+                ans[c]=-1;
+            }
+            c++;
+        }
+        return ans;
     }
 }
