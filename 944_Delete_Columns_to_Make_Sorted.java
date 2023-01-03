d2475c63faff1b9a738a7650b089acc8f0f32c59
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2023-01-03 13:42:02 +0530

    Create 944_Delete_Columns_to_Make_Sorted.java

---
+++ 944_Delete_Columns_to_Make_Sorted.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int k=strs[0].length();
+        int ans=0; //variable two store answer
+        for(int col=0;col<k;col++)
+        {
+            for(int row=1;row<strs.length;row++)
+            {
+                if(strs[row].charAt(col)<strs[row-1].charAt(col))
+                {
+                    ans+=1;
+                    break;
+                }
+            }
+        }
+        return ans;
+
+    }
+}
