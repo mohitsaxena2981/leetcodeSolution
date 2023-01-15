e822e3751383760ff78bf568f4ff7bf4aadff8a9
mohitsaxena2981 <95016118+mohitsaxena2981@users.noreply.github.com>
2023-01-15 14:26:46 +0530

    Update 704_Binary_Search.java

--- 704_Binary_Search.java
+++ 704_Binary_Search.java
@@ -1,11 +1,23 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        int size=nums.length;
-        for(int i=0;i<size;i++){
-            if(nums[i]==target){
-                return i;
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low=1;
+        int res=0;
+        int high=n;
+        while(low<=high)
+        {
+            int mid=low+((high-low)/2);
+            if(isBadVersion(mid)==true){
+                res=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        return -1;
+        return res;
+        
     }
 }
